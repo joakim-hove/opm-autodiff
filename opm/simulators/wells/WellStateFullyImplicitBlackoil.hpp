a614a1ec4a883db153b7c82531bcde5a1eedf71b
Joakim Hove <joakim.hove@gmail.com>
2021-04-24 08:32:53 +0200

    Step 22

--- opm/simulators/wells/WellStateFullyImplicitBlackoil.hpp
+++ opm/simulators/wells/WellStateFullyImplicitBlackoil.hpp
@@ -432,10 +432,6 @@ namespace Opm
 
 
 
-        void setCurrentGroupInjectionPotentials(const std::string& groupName, const std::vector<double>& pot ) {
-            this->group_state.update_injection_potentials(groupName, pot);
-        }
-
         const std::vector<double>& currentGroupInjectionPotentials(const std::string& groupName) const {
             return this->group_state.injection_potentials(groupName);
         }
